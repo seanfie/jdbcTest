aa3da1831ca8935d4e466499fc769159799c225b
MR.MA <861548313@qq.com>
2019-06-30 23:18:11 +0800

    work git

---
+++ src/com/neuedu/mainbtest/TestMain.java
@@ -0,0 +1,10 @@
+package com.neuedu.mainbtest;
+
+public class TestMain {
+    public static void main(String[] args) {
+        System.out.println("test01");
+        System.out.println("test02");
+
+
+    }
+}
